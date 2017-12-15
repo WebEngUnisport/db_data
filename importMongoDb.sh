6eb22417ae9567edf2fc9d9a3084c99ac6c98455
Lars Vögtlin <lvoegtlin@gmail.com>
2017-12-15 15:37:04 +0100

    added uni and category to the script

---
+++ importMongoDb.sh
@@ -0,0 +1,10 @@
+docker run -d -p 27017:27017 mongo
+
+sleep 2s
+mongoimport --db unisport9 --collection courses --drop --jsonArray --file output-fribourg.json
+
+sleep 2s
+mongoimport --db unisport9 --collection universities --drop --jsonArray --file uni.json
+
+sleep 2s
+mongoimport --db unisport9 --collection categories --drop --jsonArray --file category.json
