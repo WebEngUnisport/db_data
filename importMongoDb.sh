9b16759c231e858928cf22b9c157d99bcb0eaec6
Marom <romain.flac@gmail.com>
2017-12-21 20:02:40 +0100

    update script

--- importMongoDb.sh
+++ importMongoDb.sh
@@ -1,7 +1,7 @@
 docker run -d -p 27017:27017 mongo
 
 sleep 2s
-mongoimport --db unisport9 --collection courses --drop --jsonArray --file output-fribourg.json
+mongoimport --db unisport9 --collection courses --drop --jsonArray --file output-fribourg-translated.json
 
 sleep 2s
 mongoimport --db unisport9 --collection universities --drop --jsonArray --file uni.json
@@ -10,7 +10,7 @@ sleep 2s
 mongoimport --db unisport9 --collection categories --drop --jsonArray --file category.json
 
 sleep 2s
-mongoimport --db unisport9 --collection courses --drop --jsonArray --file output-bern.json
+mongoimport --db unisport9 --collection courses --jsonArray --file output-bern-translated.json
 
 sleep 2s
-mongoimport --db unisport9 --collection courses --drop --jsonArray --file output-neuch.json
+mongoimport --db unisport9 --collection courses --jsonArray --file output-neuch-translated.json
